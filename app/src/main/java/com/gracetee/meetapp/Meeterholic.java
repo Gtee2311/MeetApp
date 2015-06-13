41e38d2d039f5f0f3eb4f535d0b28c08d78a40d8
Gtee2311 <gtee@hotmail.my>
2015-06-13 08:30:06 +0800

    Added: Parse lib

---
+++ app/src/main/java/com/gracetee/meetapp/Meeterholic.java
@@ -0,0 +1,18 @@
+package com.gracetee.meetapp;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+/**
+ * Created by Grace on 13/6/2015.
+ */
+public class Meeterholic extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "EV2EhFCDxC0KCStaex9aHtonpyFrymzLWtx9wG2x", "gTFSuR2qwRFJyPa2WOpI9UJNtk6OiAROdxy3k39Q");
+        ParseUser.enableRevocableSessionInBackground();
+    }
+
+}
